e24b5d2d016ef3e1a6614c4d4fc858b5339b4491
wamballa <steve.tyrell@gmail.com>
2019-05-08 14:06:10 +0100

    created server

--- src/Forms/SendSMS.js
+++ src/Forms/SendSMS.js
@@ -2,27 +2,42 @@ import React, { Component } from "react";
 
 export default class SendSMS extends Component {
 
-  onSubmit =(e) => {
-      e.preventDefault();
+  state = {
+    message: {
+      to: '',
+      body: ''
+    }
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
     console.log("send smsm class function");
     // Twilio Credentials
-    const accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    const authToken = "your_auth_token";
+    const accountSid = "AC3d5917544c770a44dac8526f5cea1adb";
+    const authToken = "aba970d6507a1cf47dfd0e7fd1af3ca5";
+
+    fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.message)
+    });
 
     // require the Twilio module and create a REST client
-    const client = require("twilio")(accountSid, authToken);
+    // const client = require("twilio")(accountSid, authToken);
 
-    client.messages.create(
-      {
-        to: "+15558675310",
-        from: "+15017122661",
-        body: "This is the ship that made the Kessel Run in fourteen parsecs?"
-      },
-      (err, message) => {
-        console.log(message.sid);
-      }
-    );
-    return false;
+    // client.messages.create(
+    //   {
+    //     to: "+15558675310",
+    //     from: "+15017122661",
+    //     body: "This is the ship that made the Kessel Run in fourteen parsecs?"
+    //   },
+    //   (err, message) => {
+    //     console.log(message.sid);
+    //   }
+    // );
+    // return false;
   };
 
   render() {
@@ -30,7 +45,7 @@ export default class SendSMS extends Component {
       // <div>{this.SendSMS()}
 
       <div >
-        <form onSubmit={this.onSubmit()}>
+        <form onSubmit={this.onSubmit}>
           <label htmlFor="to">To:</label>
           <input
             type="tel"
